10ac5f417d3a2c3e42764db99c2d13dc102005eb
@Jeremyobrien <jjob1391@gmail.com>
2021-07-14 16:36:36 -0400

    completed half of fieldset 'basic-info'

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//'Name' field receives default focus
+const nameField = document.querySelector('#name');
+nameField.focus();
+//Default hide 'otherJobRole' field
+const otherJobRole= document.querySelector('#other-job-role');
+otherJobRole.style.display = 'none';
+//Event listener to hide or show 'otherJobRole' field
+const jobRole = document.querySelector('#title');
+jobRole.addEventListener('change', (e) =>{
+       if(e.target.value == 'other'){
+        otherJobRole.style.display = 'block';
+        } else {
+            otherJobRole.style.display= 'none';
+        }
+});
+
